f0d2ed0f5692a7e36660dbbd9839978a978c7d7c
David Horès <hores.d@gmail.com>
2026-06-05 12:33:24 +0200

    Set PHPStan rule level to 10 in phpstan.dist.neon and update index.php

--- public/index.php
+++ public/index.php
@@ -4,4 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return fn(array $context): Kernel => new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return static function (array $context) {
+    /** @var string $env */
+    $env = $context['APP_ENV'] ?? 'dev';
+
+    return new Kernel($env, (bool) $context['APP_DEBUG']);
+};
