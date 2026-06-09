4662caca98ea4b2ad9dec47a2ff66654cc6a9e48
David Horès <hores.d@gmail.com>
2026-06-09 17:03:09 +0200

    Update route and index method of Admin/UserController and admin and admin user index templates

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
-    #[Route('/admin/user', name: 'app_admin_user')]
-    public function index(): Response
+    public function __construct(
+        private readonly UserRepository $userRepository,
+    ) {}
+
+    #[Route('/admin/user', name: 'admin_user_index', methods: ['GET'])]
+    public function index(Request $request): Response
     {
+        $page = $request->query->getInt('page', 1);
+        $limit = 25;
+
+        $users = $this->userRepository->findByRole(
+            'ROLE_ADMIN',
+            false,
+            $limit,
+            $limit * ($page - 1)
+        );
+
+        $total = $this->userRepository->countByRole('ROLE_ADMIN', false);
+
         return $this->render('admin/user/index.html.twig', [
-            'controller_name' => 'Admin/UserController',
+            'users' => $users,
+            'total' => $total,
+            'page' => $page,
+            'limit' => $limit,
         ]);
     }
 }
