2fed5179430fa433ace1f17a526ece93e0a746c4
David Horès <hores.d@gmail.com>
2026-06-15 18:14:05 +0200

    Create testNavigation and pathProvider methods in HomeControllerTest

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -16,6 +16,21 @@ final class HomeControllerTest extends WebTestCase
     }
 
     /**
+     * @return iterable<array<string>>
+     */
+    public static function pathProvider(): iterable
+    {
+        yield 'Home' => ['/'];
+        yield 'Guests' => ['/guests'];
+        yield 'First visible guest' => ['/guests/2'];
+        yield 'Last visible guest' => ['/guests/100'];
+        yield 'Portfolio' => ['/portfolio'];
+        yield 'First album in the portfolio' => ['/portfolio/1'];
+        yield 'Last album in the portfolio' => ['/portfolio/5'];
+        yield 'About' => ['/about'];
+    }
+
+    /**
      * @return iterable<array<string|null>>
      */
     public static function pageProvider(): iterable
@@ -29,6 +44,58 @@ final class HomeControllerTest extends WebTestCase
         yield 'Last album in the portfolio' => ['/portfolio/5', 'Portfolio'];
         yield 'About' => ['/about', 'Qui suis-je ?', 'about-description'];
         yield 'Login' => ['/login', 'Connexion'];
+    }
+
+    /**
+     * @param string $path
+     */
+    #[DataProvider('pathProvider')]
+    public function testNavigation(string $path): void
+    {
+        $crawler = $this->client->request('GET', $path);
+        $homeLink = $crawler->filter('a[href="/"]');
+        $navLinks = $crawler->filter('.nav-link');
+
+        self::assertResponseIsSuccessful();
+        self::assertCount(1, $homeLink);
+        self::assertCount(4, $navLinks);
+
+        self::assertSelectorExists(
+            'a[href="/"] > img',
+            "Le logo est présent dans le lien vers la page d'accueil."
+        );
+
+        $logoAlt = $homeLink->filter('img')->attr('alt');
+
+        self::assertSame('Ina Zaoui', $logoAlt);
+
+        $expectedNav = [
+            'Invités' => '/guests',
+            'Portfolio' => '/portfolio',
+            'Qui suis-je ?' => '/about',
+            'Connexion' => '/login',
+        ];
+
+        $navLabels = $navLinks->each(fn($node) => trim($node->text()));
+        $navUrls = $navLinks->each(fn($node) => $node->attr('href'));
+
+        $i = 0;
+
+        foreach ($expectedNav as $label => $url) {
+            self::assertSame(
+                $label,
+                $navLabels[$i],
+                "Le lien '$label' est bien placé dans le menu de navigation."
+            );
+
+            self::assertSame(
+                $url,
+                $navUrls[$i],
+                "Le lien '$label' renvoie vers '$url'."
+            );
+
+            $i++;
+        }
     }
 
     /**
