2d9274a49da3d37c43037fc1062832a5d4850004
David Horès <hores.d@gmail.com>
2026-06-17 14:48:53 +0200

    Create testMedia method in MediaTest

--- tests/Entity/MediaTest.php
+++ tests/Entity/MediaTest.php
@@ -2,12 +2,37 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Album;
+use App\Entity\Media;
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class MediaTest extends TestCase
+final class MediaTest extends TestCase
 {
-    public function testSomething(): void
+    public function testMedia(): void
     {
-        $this->assertTrue(true);
+        $media = new Media();
+
+        $album = new Album();
+        $media->setAlbum($album);
+
+        self::assertSame($album, $media->getAlbum());
+
+        $user = new User();
+        $media->setUser($user);
+
+        self::assertSame($user, $media->getUser());
+
+        $media->setPath('uploads/image.webp');
+        self::assertSame('uploads/image.webp', $media->getPath());
+
+        $media->setTitle('Média');
+        self::assertSame('Média', $media->getTitle());
+
+        $file = self::createStub(UploadedFile::class);
+        $media->setFile($file);
+
+        self::assertSame($file, $media->getFile());
     }
 }
