3582329b1c57fcedcfe2859e4c2c9b3677a6b22a
David Horès <hores.d@gmail.com>
2026-06-17 14:54:39 +0200

    Create testUser and testUserWithoutDesc methods in UserTest

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -2,12 +2,51 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Media;
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
-class UserTest extends TestCase
+final class UserTest extends TestCase
 {
-    public function testSomething(): void
+    public function testUser(): void
     {
-        $this->assertTrue(true);
+        $user = new User();
+        $user->setEmail('email@user.com');
+
+        self::assertSame('email@user.com', $user->getEmail());
+
+        $user->setRoles(['ROLE_ADMIN']);
+        $roles = $user->getRoles();
+
+        self::assertContains('ROLE_ADMIN', $roles);
+        self::assertContains('ROLE_USER', $roles);
+
+        $user->setPassword('password');
+        self::assertSame('password', $user->getPassword());
+
+        $user->setName('Utilisateur');
+        self::assertSame('Utilisateur', $user->getName());
+
+        $user->setDescription('Description');
+        self::assertSame('Description', $user->getDescription());
+
+        $media = new Media();
+        $user->addMedia($media);
+
+        self::assertCount(1, $user->getMedias());
+        self::assertSame($user, $media->getUser());
+
+        $user->removeMedia($media);
+
+        self::assertCount(0, $user->getMedias());
+        self::assertNull($media->getUser());
+    }
+
+    public function testUserWithoutDesc(): void
+    {
+        $user = new User();
+        $user->setDescription(null);
+
+        self::assertNull($user->getDescription());
     }
 }
